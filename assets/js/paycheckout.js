7345539c4db0888d40c14c99fe49028759dcae88
k.verschoor@pay.nl <k.verschoor@pay.nl>
2021-09-23 15:52:49 +0200

    Add check to keyup

--- assets/js/paycheckout.js
+++ assets/js/paycheckout.js
@@ -1,15 +1,31 @@
 jQuery(function ($) {
-    $(window).on('load', function () {              
-        $(".woocommerce-billing-fields__field-wrapper").find('input').change(function () {       
-            $.ajax({
-                type:		'POST',
-                url:		wc_checkout_params.checkout_url,
-                data:		$('form.checkout').serialize() + '&updatecustomer=true',
-                dataType:   'json',
-                success:	function( result ) {                  
-                    $(document.body).trigger('update_checkout');
+    currentValues = {};
+    $(window).on('load', function () {
+        $(".woocommerce-billing-fields__field-wrapper").find('input').on("keyup change", function (event) {
+            refresh = true;
+            id = $(this).attr('id');
+            value = $(this).val()
+            if (id in currentValues && event.type == 'keyup') {
+                if (
+                    (currentValues[id].length > 0 && value.length > 0) ||
+                    (currentValues[id].length == 0 && value.length == 0)
+                ) {
+                    refresh = false;
                 }
-            });
-        }); 
+            }
+            if (refresh) {
+                $.ajax({
+                    type: 'POST',
+                    url: wc_checkout_params.checkout_url,
+                    data: $('form.checkout').serialize() + '&updatecustomer=true',
+                    dataType: 'json',
+                    success: function (result) {
+                        $(document.body).trigger('update_checkout');
+                        currentValues[id] = value;
+                    }
+                });
+            }
+
+        });
     });
 });
