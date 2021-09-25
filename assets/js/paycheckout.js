c6bf4effb4aeb6ce22964814b79f00c8b46999dd
woutse <wouter@pay.nl>
2021-09-25 13:54:53 +0200

    Updated js

--- assets/js/paycheckout.js
+++ assets/js/paycheckout.js
@@ -1,31 +1,13 @@
-jQuery(function ($) {
-    currentValues = {};
-    $(window).on('load', function () {
-        $(".woocommerce-billing-fields__field-wrapper").find('input').on("keyup change", function (event) {
-            refresh = true;
-            id = $(this).attr('id');
-            value = $(this).val()
-            if (id in currentValues && event.type == 'keyup') {
-                if (
-                    (currentValues[id].length > 0 && value.length > 0) ||
-                    (currentValues[id].length == 0 && value.length == 0)
-                ) {
-                    refresh = false;
-                }
+jQuery(document).ready(function () {
+    jQuery(".woocommerce-billing-fields__field-wrapper").find('input[name*="company"]').on("blur", function (event) {
+        jQuery.ajax({
+            type: 'POST',
+            url: wc_checkout_params.checkout_url,
+            data: jQuery('form.checkout').serialize() + '&updatecustomer=true',
+            dataType: 'json',
+            success: function (result) {
+                jQuery(document.body).trigger('update_checkout');
             }
-            if (refresh) {
-                $.ajax({
-                    type: 'POST',
-                    url: wc_checkout_params.checkout_url,
-                    data: $('form.checkout').serialize() + '&updatecustomer=true',
-                    dataType: 'json',
-                    success: function (result) {
-                        $(document.body).trigger('update_checkout');
-                        currentValues[id] = value;
-                    }
-                });
-            }
-
         });
     });
 });
