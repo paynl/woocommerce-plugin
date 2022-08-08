9c493316aa51c62c106e0d6e3c2a1422f046a522
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-08-08 16:40:05 +0200

    PLUG-1074 - Fix table not being created when reinstalled

--- includes/classes/PPMFWC/Setup.php
+++ includes/classes/PPMFWC/Setup.php
@@ -17,43 +17,63 @@ class PPMFWC_Setup
         $table_name_options = $wpdb->prefix . "pay_options";
         $table_name_option_subs = $wpdb->prefix . "pay_option_subs";
 
+        $check_tables = [
+            $table_name_transactions => ($wpdb->get_var("SHOW TABLES LIKE '$table_name_transactions'") == $table_name_transactions),
+            $table_name_options => ($wpdb->get_var("SHOW TABLES LIKE '$table_name_options'") == $table_name_options),
+            $table_name_option_subs => ($wpdb->get_var("SHOW TABLES LIKE '$table_name_option_subs'") == $table_name_option_subs)
+        ];
 
-        if (empty($pay_db_version)) {
-            $sqlTransactions = "CREATE TABLE `$table_name_transactions` (
-            `id` int(11) NOT NULL AUTO_INCREMENT,
-            `transaction_id` varchar(50) NOT NULL,
-            `option_id` int(11) NOT NULL,
-            `option_sub_id` int(11) DEFAULT NULL,
-            `amount` int(11) NOT NULL,
-            `order_id` bigint(20) NOT NULL,
-            `status` varchar(10) NOT NULL DEFAULT 'PENDING',
-            `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `last_update` datetime DEFAULT NULL,
-            `start_data` text NOT NULL,
-            PRIMARY KEY (`id`),
-            UNIQUE KEY `{$table_name_transactions}_transaction_id` (`transaction_id`)
-          );";
-            $sqlOptions = "CREATE TABLE `$table_name_options` (
-              `id` int(10) unsigned NOT NULL COMMENT 'Payment option Id',
-              `name` varchar(255) NOT NULL COMMENT 'Payment option name',
-              `image` varchar(255) NOT NULL COMMENT 'The url to the icon image',
-              `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The datetime this payment option was refreshed',
-              PRIMARY KEY (`id`)
-            );";
-            $sqlOptionSub = "CREATE TABLE `$table_name_option_subs` (
-            `option_id` int(10) unsigned NOT NULL COMMENT 'Payment option Id',
-            `option_sub_id` int(10) unsigned NOT NULL COMMENT 'Payment option sub Id',  
-            `name` varchar(255) NOT NULL COMMENT 'The name of the option sub',
-            `image` varchar(255) NOT NULL COMMENT 'The url to the icon image',
-            `active` tinyint(1) NOT NULL COMMENT 'OptionSub  active or not',
-            PRIMARY KEY (`option_id`, option_sub_id)
-          );";
+        if (empty($pay_db_version) || in_array(false, $check_tables)) {
+            $sqlTransactions = '';
+            if ($check_tables[$table_name_transactions] === false) {
+                $sqlTransactions = "CREATE TABLE `$table_name_transactions` (
+                    `id` int(11) NOT NULL AUTO_INCREMENT,
+                    `transaction_id` varchar(50) NOT NULL,
+                    `option_id` int(11) NOT NULL,
+                    `option_sub_id` int(11) DEFAULT NULL,
+                    `amount` int(11) NOT NULL,
+                    `order_id` bigint(20) NOT NULL,
+                    `status` varchar(10) NOT NULL DEFAULT 'PENDING',
+                    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    `last_update` datetime DEFAULT NULL,
+                    `start_data` text NOT NULL,
+                    PRIMARY KEY (`id`),
+                    UNIQUE KEY `{$table_name_transactions}_transaction_id` (`transaction_id`)
+                );";
+            }
+
+            $sqlOptions = '';
+            if ($check_tables[$table_name_options] === false) {
+                $sqlOptions = "CREATE TABLE `$table_name_options` (
+                    `id` int(10) unsigned NOT NULL COMMENT 'Payment option Id',
+                    `name` varchar(255) NOT NULL COMMENT 'Payment option name',
+                    `image` varchar(255) NOT NULL COMMENT 'The url to the icon image',
+                    `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The datetime this payment option was refreshed',
+                    PRIMARY KEY (`id`)
+                );";
+            }
+
+            $sqlOptionSub = '';
+            if ($check_tables[$table_name_option_subs] === false) {
+                $sqlOptionSub = "CREATE TABLE `$table_name_option_subs` (
+                    `option_id` int(10) unsigned NOT NULL COMMENT 'Payment option Id',
+                    `option_sub_id` int(10) unsigned NOT NULL COMMENT 'Payment option sub Id',  
+                    `name` varchar(255) NOT NULL COMMENT 'The name of the option sub',
+                    `image` varchar(255) NOT NULL COMMENT 'The url to the icon image',
+                    `active` tinyint(1) NOT NULL COMMENT 'OptionSub  active or not',
+                    PRIMARY KEY (`option_id`, option_sub_id)
+                );";
+            }
+
             $sql = $sqlTransactions . "\n" . $sqlOptions . "\n" . $sqlOptionSub;
 
             dbDelta($sql);
+            if (empty($pay_db_version)) {
+                add_option("pay_db_version", self::db_version);
+            }
+        }
 
-            add_option("pay_db_version", self::db_version);
-        } elseif ($pay_db_version < 1.1) {
+        if ($pay_db_version < 1.1) {
             $sql = "ALTER TABLE `$table_name_transactions` MODIFY order_id BIGINT(20);";
 
             $wpdb->query($sql);
@@ -138,5 +158,4 @@ class PPMFWC_Setup
             }
         }
     }
-
 }
