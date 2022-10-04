01d948ba2b1bf9277b9c49d178cc03bb226bff55
Anne <anne@pay.nl>
2022-10-04 17:05:09 +0200

    deprecated

--- includes/classes/PPMFWC/Gateway/AfterpayInternational.php
+++ includes/classes/PPMFWC/Gateway/AfterpayInternational.php
@@ -10,7 +10,7 @@ class PPMFWC_Gateway_AfterpayInternational extends PPMFWC_Gateway_Abstract
 
     public static function getName()
     {
-        return 'Riverty';
+        return 'Riverty (deprecated)';
     }
 
     public static function getOptionId()
