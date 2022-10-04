d43c8281b98fc42b26f6afc8a5d826de6da8dcac
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-10-04 14:50:11 +0200

    PLUG-1160 - Multi Processing Prevention

--- includes/classes/PPMFWC/Helper/Transaction.php
+++ includes/classes/PPMFWC/Helper/Transaction.php
@@ -258,5 +258,40 @@ class PPMFWC_Helper_Transaction
 
         return $newStatus;
     }
+    
+    public static function checkProcessing($transactionId)
+    {
+        global $wpdb;
 
+        $table_name_processing = $wpdb->prefix . "pay_processing";
+        try {
+            $result = $wpdb->get_results(
+                $wpdb->prepare("SELECT * FROM $table_name_processing WHERE transaction_id = %s AND created > date_sub('%s', interval 1 minute)", $transactionId, date('Y-m-d H:i:s')),
+                ARRAY_A
+            );
+            if (empty($result[0])) {
+                $wpdb->replace(
+                    $table_name_processing,
+                    array(
+                        'transaction_id' => $transactionId,
+                        'created' =>  date('Y-m-d H:i:s')
+                    ),
+                    array(
+                        '%s', '%s'
+                    )
+                );
+            }
+        } catch (\Exception $e) {
+            $result = array();
+        }
+
+        return is_array($result) ? $result : array();
+    }
+
+    public static function removeProcessing($transactionId)
+    {
+        global $wpdb;
+        $table_name_processing = $wpdb->prefix . "pay_processing";
+        $wpdb->delete($table_name_processing, array('transaction_id' => $transactionId), array('%s'));
+    } 
 }
