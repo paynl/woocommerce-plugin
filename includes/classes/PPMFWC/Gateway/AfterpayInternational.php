3aefffa953fe260a14e8fd849add588e12725856
Anne <anne@pay.nl>
2022-10-04 09:31:46 +0200

    Remove international

--- includes/classes/PPMFWC/Gateway/AfterpayInternational.php
+++ includes/classes/PPMFWC/Gateway/AfterpayInternational.php
@@ -10,7 +10,7 @@ class PPMFWC_Gateway_AfterpayInternational extends PPMFWC_Gateway_Abstract
 
     public static function getName()
     {
-        return 'Riverty International';
+        return 'Riverty';
     }
 
     public static function getOptionId()
