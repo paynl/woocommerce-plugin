3316dd346846c3b16a5b0b9490f6a6c2740af7c8
woutse <wouter@pay.nl>
2022-11-02 11:34:37 +0100

    Updated versions

--- includes/classes/PPMFWC/Helper/Data.php
+++ includes/classes/PPMFWC/Helper/Data.php
@@ -277,7 +277,7 @@ class PPMFWC_Helper_Data
      */
     public static function getVersion()
     {
-        return '3.11.1';
+        return '3.12.0';
     }
 
     /**
--- woocommerce-payment-paynl.php
+++ woocommerce-payment-paynl.php
@@ -4,12 +4,10 @@
  * Plugin Name: PAY. Payment Methods for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/woocommerce-paynl-payment-methods/
  * Description: PAY. Payment Methods for WooCommerce
- * Version: 3.11.1
+ * Version: 3.12.0
  * Author: PAY.
  * Author URI: https://www.pay.nl
  * Requires at least: 3.5.1
- * Tested up to: 6.0.3
- * WC tested up to: 7.0.0
  * WC requires at least: 3.0
  *
  * Text Domain: woocommerce-paynl-payment-methods
