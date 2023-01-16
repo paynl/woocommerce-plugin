e453d82623fa79e6c016e4aff1d157d8495c5cc9
woutse <wouter@pay.nl>
2023-01-16 15:57:19 +0100

    Added country filter

--- includes/classes/PPMFWC/Gateway/Abstract.php
+++ includes/classes/PPMFWC/Gateway/Abstract.php
@@ -161,7 +161,7 @@ abstract class PPMFWC_Gateway_Abstract extends WC_Payment_Gateway
                 'title'       => esc_html(__('Country', PPMFWC_WOOCOMMERCE_TEXTDOMAIN)),
                 'type'        => 'multiselect',
                 'options'     => array_merge(array('all' => esc_html(__('Available for all countries', PPMFWC_WOOCOMMERCE_TEXTDOMAIN))), WC()->countries->get_countries()),
-                'default'     => 'none',
+                'default'     => 'all',
                 'description' => sprintf(esc_html(__('Select one or more billing-countries for which %s should be availble', PPMFWC_WOOCOMMERCE_TEXTDOMAIN)), $this->getName() ),
                 'desc_tip'    => esc_html(__('Select in which (billing) country this method should be available.', PPMFWC_WOOCOMMERCE_TEXTDOMAIN)),
                 'class'       => 'countryLimit'
