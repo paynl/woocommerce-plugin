69cdccffa15e1c9066ee51d53ed46ddb19c51a1c
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2023-01-20 16:29:43 +0100

    Update Transaction.php

--- includes/classes/PPMFWC/Helper/Transaction.php
+++ includes/classes/PPMFWC/Helper/Transaction.php
@@ -15,7 +15,7 @@ class PPMFWC_Helper_Transaction
         $arrStatus['authorised'] = get_option('paynl_status_authorized');
         $arrStatus['verify'] = get_option('paynl_status_verify');
 
-        return $arrStatus[$payStatus] === false ? $payStatus : $arrStatus[$payStatus];
+        return empty($arrStatus[$payStatus]) ? $payStatus : $arrStatus[$payStatus];
     }
 
     public static function newTransaction($transactionId, $opionId, $amount, $orderId, $startData, $optionSubId = null)
