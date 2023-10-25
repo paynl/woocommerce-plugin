aa034435e6df9085c8bae3984686da06d5265062
woutse <wouter@pay.nl>
2023-10-25 16:50:36 +0200

    Added birthdate, coc and vat fields

--- assets/js/paynl-blocks.js
+++ assets/js/paynl-blocks.js
@@ -1,46 +1,61 @@
 const PaynlComponent = (props) =>{
     let {image_path, useEffect, gateway} = props
     const [ selectedIssuer, selectIssuer ] = wp.element.useState('');
+    const [ cocNumber, setCocNumber ] = wp.element.useState('');
+    const [ vatNumber, setVatNumber ] = wp.element.useState('');
+    const [ dob, selectDate ] = wp.element.useState('');
     const { eventRegistration, emitResponse } = props;
     const { onPaymentSetup } = eventRegistration;
+    let payIssuers = gateway.issuers;
+
     useEffect(() => {
         const unsubscribe = onPaymentSetup(() => {
-            return {
+            let response = {
                 type: emitResponse.responseTypes.SUCCESS,
-                meta: {
-                    paymentMethodData: {
-                        selectedIssuer: selectedIssuer
-                    },
-                },
+                meta: {},
             };
+
+            let paymentMethodData = {};
+            paymentMethodData['selectedIssuer'] = selectedIssuer;
+            paymentMethodData['vat_number'] = vatNumber;
+            paymentMethodData['coc_number'] = cocNumber;
+            paymentMethodData[gateway.paymentMethodId + '_birthdate'] = dob;
+            response.meta.paymentMethodData = paymentMethodData;
+            return response;
         });
         return () => {
             unsubscribe();
         };
-    }, [
-        onPaymentSetup, selectedIssuer
-    ]);
+    }, [onPaymentSetup, selectedIssuer, dob, cocNumber, vatNumber]);
 
-    let i = gateway.issuers;
-
-    if (gateway.paymentMethodId == 'pay_gateway_ideal') {
-        return React.createElement('div', {className: 'PPMFWC_desc'},
+        return React.createElement('div', {className: 'PPMFWC_container'},
                 React.createElement('img', {src: image_path}, null),
-                React.createElement('span', {}, gateway.description),
+                React.createElement('span', {className: 'description'}, gateway.description),
                 React.createElement('div', {},
-                React.createElement('select',  {onChange: (e)=>{
-                        selectIssuer(e.target.value)
-                    }},
-                    React.createElement("option", {}, gateway.text_selectissuer),
-                    ...i.map(issuer => React.createElement("option", {value: issuer.option_sub_id}, issuer.name))
-            )
+                    (gateway.paymentMethodId == 'pay_gateway_ideal' ?
+                        React.createElement('select',  {onChange: (e)=>{
+                                    selectIssuer(e.target.value)
+                                }},
+                            React.createElement("option", {}, gateway.texts.selectissuer),
+                            ...payIssuers.map(issuer => React.createElement("option", {value: issuer.option_sub_id}, issuer.name))
+                        ) : ''),
+                    (gateway.showbirthdate == true ?
+                        React.createElement('div', {className: 'field'},
+                                 React.createElement('span', {}, gateway.texts.enterbirthdate + ': '),
+                                 React.createElement('input', {type: 'date', onChange: (e)=>{ selectDate(e.target.value) }})
+                        ) : '' ),
+                    (gateway.showCocField == true ?
+                    React.createElement('div', {className: 'field'},
+                        React.createElement('span', {}, gateway.texts.enterCocNumber + ': '),
+                        React.createElement('input', {type: 'text', onChange: (e)=>{ setCocNumber(e.target.value) }})
+                        ) : '' ),
+                    ( gateway.showVatField == true ?
+                        React.createElement('div', {className: 'field'},
+                            React.createElement('span', {}, gateway.texts.enterVatNumber + ': '),
+                            React.createElement('input', {type: 'text', onChange: (e)=>{ setVatNumber(e.target.value) }})
+                        ) : '' )
         ))
-    }
 
-    return  React.createElement('div', {className: 'PPMFWC_desc'},
-                React.createElement('img', {src: image_path}, null),
-                React.createElement('span', {}, gateway.description),
-    );
 }
 
 function PaynlLabel({image_path, title})
--- woocommerce-payment-paynl.php
+++ woocommerce-payment-paynl.php
@@ -65,6 +65,12 @@ if (is_plugin_active('woocommerce/woocommerce.php') || is_plugin_active_for_netw
             $blocks_js_route = PPMFWC_PLUGIN_URL . 'assets/js/paynl-blocks.js';
             $gateways = WC()->payment_gateways()->get_available_payment_gateways();
             $payGateways = [];
+
+            $texts['selectissuer'] = __('Select an issuer', PPMFWC_WOOCOMMERCE_TEXTDOMAIN);
+            $texts['enterbirthdate'] = __('Enter date of birth', PPMFWC_WOOCOMMERCE_TEXTDOMAIN);
+            $texts['enterCocNumber'] = __('Enter COC number', PPMFWC_WOOCOMMERCE_TEXTDOMAIN);
+            $texts['enterVatNumber'] = __('Enter VAT number', PPMFWC_WOOCOMMERCE_TEXTDOMAIN);
+
             foreach ($gateways as $gateway_id => $gateway) {
                 /** @var PPMFWC_Gateway_Abstract $gateway */
                 if (substr($gateway_id, 0, 11) != 'pay_gateway') {
@@ -76,7 +82,10 @@ if (is_plugin_active('woocommerce/woocommerce.php') || is_plugin_active_for_netw
                   'description' => $gateway->description,
                   'image_path' => $gateway->getIcon(),
                   'issuers' => $gateway->getIssuers(),
-                  'text_selectissuer' => __('Select an issuer', PPMFWC_WOOCOMMERCE_TEXTDOMAIN)
+                  'texts' => $texts,
+                  'showbirthdate' => $gateway->showDOB(),
+                  'showVatField' => get_option('paynl_show_vat_number') == "yes",
+                  'showCocField' => get_option('paynl_show_coc_number') == "yes"
                 );
             }
             wp_enqueue_script('paynl-blocks-js', $blocks_js_route, array('wc-blocks-registry'), (string)time(), true);
