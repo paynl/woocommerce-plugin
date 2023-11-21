8ff95f00a4a7977d9bd8a8e25045f487cc4f0bcd
Anne <anne@pay.nl>
2023-11-21 17:21:48 +0100

    Change it to In3 Business

--- includes/classes/PPMFWC/Gateway/In3business.php
+++ includes/classes/PPMFWC/Gateway/In3business.php
@@ -31,4 +31,68 @@ class PPMFWC_Gateway_In3business extends PPMFWC_Gateway_Abstract
     {
         return 3192;
     }
+
+    /**
+     * @return boolean
+     */
+    public static function showAuthorizeSetting()
+    {
+        return true;
+    }
+
+    /**
+     * @return boolean
+     */
+    public static function showDOB()
+    {
+        return true;
+    }
+
+    /**
+     * @return boolean
+     */
+    public static function useInvoiceAddressAsShippingAddress()
+    {
+        return true;
+    }
+
+    /**
+     * @return boolean
+     */
+    public static function alternativeReturnURL()
+    {
+        return true;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function askBirthdate()
+    {
+        return $this->get_option('ask_birthdate') != 'no';
+    }
+
+    /**
+     * @return boolean
+     */
+    public function birthdateRequired()
+    {
+        return ($this->get_option('ask_birthdate') == 'yes_required' || ($this->get_option('ask_birthdate') == 'yes' && $this->get_option('birthdate_required') == 'yes')); // phpcs:ignore
+    }
+
+    /**
+     * @return boolean
+     */
+    public function showVat()
+    {
+        return get_option('paynl_show_vat_number') == "yes";
+    }
+
+    /**
+     * @return boolean
+     */
+    public function showCoc()
+    {
+        return get_option('paynl_show_coc_number') == "yes";
+    }
 }
