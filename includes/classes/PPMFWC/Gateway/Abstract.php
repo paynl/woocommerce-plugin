cd210c5553d7512222ee013f4ab2443c41bb0448
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2024-04-03 15:55:12 +0200

    PLUG-3313 - Fix address for postNL plugin

--- includes/classes/PPMFWC/Gateway/Abstract.php
+++ includes/classes/PPMFWC/Gateway/Abstract.php
@@ -648,10 +648,17 @@ abstract class PPMFWC_Gateway_Abstract extends WC_Payment_Gateway
         $startData['enduser'] = $enduser;
 
         # Retrieve order data
-        $shippingAddress = $order->get_shipping_address_1() . ' ' . $order->get_shipping_address_2();
-        $billingAddress  = $order->get_billing_address_1() . ' ' . $order->get_billing_address_2();
-        $aBillingAddress = \Paynl\Helper::splitAddress($billingAddress);
+        $shippingAddress = $order->get_shipping_address_1();
+        $shippingAddress .= ' ';
+        $shippingAddress .= (!empty($order->get_meta('_shipping_house_number'))) ? $order->get_meta('_shipping_house_number') : '';
+        $shippingAddress .= $order->get_shipping_address_2();
 
+        $billingAddress = $order->get_billing_address_1();
+        $billingAddress .= ' ';
+        $billingAddress .= (!empty($order->get_meta('_billing_house_number'))) ? $order->get_meta('_billing_house_number') : '';
+        $billingAddress .= $order->get_billing_address_2();
+        
+        $aBillingAddress = \Paynl\Helper::splitAddress($billingAddress);$aBillingAddress = \Paynl\Helper::splitAddress($billingAddress);
         $aShippingAddress = \Paynl\Helper::splitAddress($shippingAddress);
         $address = array(
             'streetName' => $aShippingAddress[0],
