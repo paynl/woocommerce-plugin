52e85ed37699e43512cd56be0dc67b07c4392cb6
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2024-10-08 10:57:47 +0200

    Fix product page JS

--- assets/js/payfastcheckout.js
+++ assets/js/payfastcheckout.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
-    jQuery('.pay-quick-checkout-product .pay-quick-checkout').click(function (e) {
+    jQuery('.pay-fast-checkout-product .pay-fast-checkout').click(function (e) {
         e.preventDefault();   
         window.location.href = "/?wc-api=Wc_Pay_Gateway_Fccreate&source=product&product_id=" + jQuery('input[name="fast-checkout-product-id"]').val() + "&variation_id=" + jQuery('input[name="variation_id"]').val() + "&quantity=" + jQuery('input[name="quantity"]').val();
     });
