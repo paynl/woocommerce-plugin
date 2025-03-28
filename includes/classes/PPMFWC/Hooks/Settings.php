a2facaff90ee0ce3841f4947c1efa408c3e99ffb
woutse <wouter@pay.nl>
2025-03-28 10:20:38 +0100

    Fixed possible fatal error fetching address

--- includes/classes/PPMFWC/Hooks/Settings.php
+++ includes/classes/PPMFWC/Hooks/Settings.php
@@ -21,8 +21,14 @@ class PPMFWC_Hooks_Settings
      */
     public static function ppmfwc_settings_email($recipient, $order, $email)
     {
-        # Setting check: dont send emails in case of B2B
-        if (!empty($order->get_address('billing')['company'])) {
+        # Setting check: don't send emails in case of B2B
+        if (!$order instanceof WC_Order) {
+            return $recipient;
+        }
+
+        $billing_address = $order->get_address('billing');
+
+        if (is_array($billing_address) && !empty($billing_address['company'])) {
             $billink_gateway = PPMFWC_Gateways::ppmfwc_getGateWayById(1672);
             if ($order->get_payment_method() == $billink_gateway->getId()) {
                 if (!empty($billink_gateway->settings['b2b_invoices_disabled']) && $billink_gateway->settings['b2b_invoices_disabled'] == 'yes') {
