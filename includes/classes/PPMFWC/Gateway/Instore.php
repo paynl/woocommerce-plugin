1102369d77f295de91a6b025bc5f3828b8c83800
Stan van Hoorn <9131625+StanvanHoorn@users.noreply.github.com>
2025-10-13 11:10:11 +0200

    Fix method ID of Instore paymend method
    Corrected incorrect payment method id.
    
    See: https://developer.pay.nl/docs/payment-option-ids-subids#in-person-payments-card-present

--- includes/classes/PPMFWC/Gateway/Instore.php
+++ includes/classes/PPMFWC/Gateway/Instore.php
@@ -101,7 +101,7 @@ class PPMFWC_Gateway_Instore extends PPMFWC_Gateway_Abstract
      */
     public static function getOptionId()
     {
-        return 1729;
+        return 1927;
     }
 
     /**
