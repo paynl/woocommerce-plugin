086e31af39aac56a1c903bad0aec5853b40b22d7
Anne <anne@pay.nl>
2025-12-01 13:27:58 +0100

    requested change

--- includes/classes/PPMFWC/Gateway/Abstract.php
+++ includes/classes/PPMFWC/Gateway/Abstract.php
@@ -73,8 +73,7 @@ abstract class PPMFWC_Gateway_Abstract extends WC_Payment_Gateway
         }
 
         // if any of the above settings are not set use static.pay for checkout images
-        $fallback = 'https://static.pay.nl/payment_profiles/100x100/' . $this->getOptionId() . '.png';
-        return $fallback;
+        return 'https://static.pay.nl/payment_profiles/100x100/' . $this->getOptionId() . '.png';
     }
 
     /**
